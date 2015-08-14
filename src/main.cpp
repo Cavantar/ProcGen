d4e380a6fc79476733bbba0d97e0b432cc2095d6
jakubb126 <jakubb126@gmail.com>
2015-08-14 09:26:52 +0200

    First commit from magit !!

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,8 @@ int main(int argc, char **argv)
     
     std::cout << expression << " = " << result << std::endl;
     
+    // Testing safdfasdff
+
   } while (expression != "exit");
   
   system("pause");
